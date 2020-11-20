9c72708375de03667aa4447acfe9282cb216ac86
wanghaidong01 <wanghaidong01@meicai.cn>
2020-11-20 11:13:06 +0800

    添加账单信息

---
+++ src/main/java/com/caffe/algorithm/learn/Test.java
@@ -0,0 +1,12 @@
+package com.caffe.algorithm.learn;
+
+/**
+ * @author BitterCaffe
+ * @date 2020/11/20
+ * @description: TODO
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("ok ……");
+    }
+}
